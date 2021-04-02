44fbae83ba7269607372d0cb23eed17d6ab3c544
Matheus Kroning <kroningmatheus@gmail.com>
2021-04-02 20:42:39 -0300

    :zap: Adding Assest and configuration
    Adding Assest and configuration

--- App.tsx
+++ App.tsx
@@ -1,21 +1,87 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { LandingScreen } from './src/screens/LandingScreen';
+import { HomeScreen } from './src/screens/HomeScreen';
+
+import { createStackNavigator } from 'react-navigation-stack'
+import { createAppContainer, createSwitchNavigator } from 'react-navigation'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+
+
+const switchNavigator = createStackNavigator({
+
+  LandingScreen: {
+    screen: createStackNavigator({
+      Landing: LandingScreen
+    },{
+      defaultNavigationOptions: {
+        headerShown: false
+      }
+    })
+  },
+  //cria o menu inferior
+  homeStack : createBottomTabNavigator({
+    // Home tab Icon
+    home: {
+      screen: createStackNavigator({
+        HomePage: HomeScreen
+      }),
+      navigationOptions: {
+        tabBarIcon: ({ focused, tintColor}) => {
+          let icon = focused == true ? require('./src/images/home.png'): require('./src/images/home.png')
+          return <Image source={icon} style={styles.tabIcon} />
+        }
+      }
+    },
+    // Offet tab Icon
+    Offer: {
+      screen: createStackNavigator({
+        OfferPage: HomeScreen
+      }),
+      navigationOptions: {
+        tabBarIcon: ({ focused, tintColor}) => {
+          let icon = focused == true ? require('./src/images/offer_icon.png'): require('./src/images/offer_n_icon.png')
+          return <Image source={icon} style={styles.tabIcon} />
+        }
+      }
+    },
+    // Cart tab Icon
+    // Cart: {
+    //   screen: createStackNavigator({
+    //     CartPage: HomeScreen
+    //   }),
+    //   navigationOptions: {
+    //     tabBarIcon: ({ focused, tintColor}) => {
+    //       let icon = focused == true ? require('./src/images/.png'): require('./src/images/home_n_icon.png')
+    //       return <Image source={icon} style={styles.tabIcon} />
+    //     }
+    //   }
+    // },
+    Account: {
+      screen: createStackNavigator({
+        AccountPage: HomeScreen
+      }),
+      navigationOptions: {
+        tabBarIcon: ({ focused, tintColor}) => {
+          let icon = focused == true ? require('./src/images/account_icon.png'): require('./src/images/account_n_icon.png')
+          return <Image source={icon} style={styles.tabIcon} />
+        }
+      }
+    }
+  })
+})
+const AppNavigation = createAppContainer(switchNavigator);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <AppNavigation />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  tabIcon: {
+    width: 30,
+    height: 30
+  }
 });
